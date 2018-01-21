4cae86e20d8400fd941f67d92f8ff8a60ad79e2b
Dsal3316 <35541000+Dsal3316@users.noreply.github.com>
2018-01-21 14:05:46 -0600

    CIS2013_Week01_Homework1
    Week 1 Homework 1

---
+++ CIS2013_Week01_Homework1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "=========================================================== \n";
+	
+	cout << " Drew Salser \n";
+	
+	cout << " Student,  Oklahoma State University - Oklahoma City \n";
+	
+	cout << " 2809 Pinto Trail                   p: (405)315-1303 \n";
+	
+	cout << " Edmond, OK                         e: Salser@okstate.edu \n";
+	
+	cout << "                                    g: github.com/Dsal3316 \n";
+	
+	cout << "                                    t: @drewasalser \n";
+	
+	cout << "=========================================================== \n";
+	
+	
+	
+	return 0;
+}
