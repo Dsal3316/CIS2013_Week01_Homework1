903b3b8b44a8d1740a9af0c7f482413db9b51f07
Dsal3316 <35541000+Dsal3316@users.noreply.github.com>
2018-01-23 00:58:16 -0600

    updated the business card.

--- CIS2013_Week01_Homework1.cpp
+++ CIS2013_Week01_Homework1.cpp
@@ -6,14 +6,19 @@ int main()
 	cout << "=========================================================== \n";
 	
 	cout << " Drew Salser \n";
+	cout << endl;
 	
 	cout << " Student,  Oklahoma State University - Oklahoma City \n";
+	cout << endl;
 	
 	cout << " 2809 Pinto Trail                   p: (405)315-1303 \n";
+	cout << endl;
 	
 	cout << " Edmond, OK                         e: Salser@okstate.edu \n";
+	cout << endl;
 	
 	cout << "                                    g: github.com/Dsal3316 \n";
+	cout << endl;
 	
 	cout << "                                    t: @drewasalser \n";
 	
